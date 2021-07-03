067bc86794b22fe128c4de6e4e80f957f1aaff2d
oppy <nettoflorestal@gmail.com>
2021-07-02 21:14:57 -0300

    Adicionei py e html

---
+++ aula.py
@@ -0,0 +1,25 @@
+print("João")
+# Comentário de código no Python é com hashtag
+# No Python a única saída de tela É O PRINT. Isso porque é um BACKEND e não precisa ter
+# interação com o usuário (ou a interação é mínima)
+ 
+
+
+# PYTHON TEM TIPAGEM FORTE
+# TESTAR O print("36"+36) PARA ENTENDER O CONCEITO ACIMA
+
+"""
+Para comentário 
+de Bloco
+no python
+"""
+
+print("My age is: ", 36)
+print("Jukka's age is: ", 13)
+print("Ellen's age is: ", 22)
+
+print("All ages sum is: ", (36+13+22))
+
+print("Mean age is: ", (36+13+22)/3 )
+
+
